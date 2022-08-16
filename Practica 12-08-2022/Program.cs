0dc2b6fc0e09dc5aac9092a4d5b8c3d08555b41e
Hazel Granados <hazelbgranados@gmail.com>
2022-08-15 20:47:02 -0900

    Agregar archivos de proyecto.

---
+++ Practica 12-08-2022/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Practica_12_08_2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su nota promedio");
+            double nota = double.Parse(Console.ReadLine());
+
+            if (nota >= object && nota <= 10)
+            {
+                if (nota >= 6)
+                {
+                    Console.WriteLine("Aprobado");
+                }
+                else if (nota >= 5)
+                {
+                    Console.WriteLine("Preposion");
+                }
+                else
+                {
+                    Console.WriteLine("Reprobado");
+                }
+            }
+            else
+            {
+                Console.WriteLine("La nota no es correcta");
+            }
+        }
+    }    
+}
