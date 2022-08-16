ffce7fd388fc50395d2af21ebb39f1d44f64bd70
Hazel Granados <hazelbgranados@gmail.com>
2022-08-15 20:52:00 -0900

    Programa 4

---
+++ App4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace App4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool condition = true;
+            int cont = 0;
+
+            Console.WriteLine("Ingrese el numero de repeticiones que desea: ");
+            int Limite = int.Parse(Console.ReadLine());
+
+            while (condition)
+            {
+                if (cont == Limite)
+                    condition = false;
+
+                Console.WriteLine(cont);
+                cont++;
+            }
+        }
+    }
+}
