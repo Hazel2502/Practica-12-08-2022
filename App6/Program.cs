48801e5f759dc0b875e4209a69ebb214e3f0e711
Hazel Granados <hazelbgranados@gmail.com>
2022-08-15 20:58:22 -0900

    Programa 7

---
+++ App6/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace App6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            //Captura de datos desde la consola
+            Console.WriteLine("Introduzca un numero");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduzca otro numero");
+            num2 = int.Parse(Console.ReadLine());
+            //Llamado de funciones locales
+            Console.WriteLine("La suma es: " + Suma(num1, num2));
+            Console.WriteLine("La resta es: " + Resta(num1, num2));
+            Console.WriteLine("La multiplicacion es: " + Multi(num1, num2));
+            Console.WriteLine("La division es: " + Div(num1, num2));
+        }
+
+        //Funcion con variable y return
+        static int Suma(int num1, int num2)
+        {
+            var suma = num1 + num2;
+            return suma;
+        }
+
+        //Funcion return directo
+        static int Resta(int num1, int num2)
+        {
+            return (num1 - num2);
+        }
+
+        //Funcion lambda
+        static int Multi(int num1, int num2) => num1 * num2;
+
+        //Funcion con conversion a double
+        static double Div(int num1, int num2)
+        {
+            double numd1 = Convert.ToDouble(num1);
+            double numd2 = Convert.ToDouble(num2);
+            var div = numd1 / numd2;
+            return div;
+        }
+    }
+}
---
+++ CALCULADORA/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace CALCULADORA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Respuesta = "";
+            do
+            {
+                int Num1, Num2;
+                int R = 0;
+
+                //Calculadora con operaciones basicas
+                Console.WriteLine("Seleccione unas de las operaciones que desea realizar: ");
+
+                Console.WriteLine(" SUMA");
+                Console.WriteLine(" RESTA");
+                Console.WriteLine(" MULTIPLICACION ");
+                Console.WriteLine(" DIVISION ");
+                Respuesta = Console.ReadLine();
+                string eleccion = Convert.ToString(Respuesta);
+
+                Console.WriteLine("Ingrese dos numero con el que desea realizar las operaciones");
+                Console.Write("Num1:  ");
+                Num1 = int.Parse(Console.ReadLine());
+                Console.Write("Num2: ");
+                Num2 = int.Parse(Console.ReadLine());
+                Console.ReadLine();
+                switch (eleccion)
+                {
+                    case "SUMA":
+                        Console.WriteLine("El resultado de la SUMA es: ");
+                        R = Num1 + Num2;
+                        Console.WriteLine("{0} + {1} = {2}", Num1, Num2, R);
+                        break;
+                    case "RESTA":
+                        Console.WriteLine("El resultado de la RESTA  es:");
+                        R = Num1 - Num2;
+                        Console.WriteLine("{0} - {1} = {2}", Num1, Num2, R);
+                        break;
+                    case "MULTIPLICACION":
+                        Console.WriteLine("El resultado de la MULTIPLICACION es:");
+                        R = Num1 * Num2;
+                        Console.WriteLine("{0} * {1} = {2}", Num1, Num2, R);
+                        break;
+                    case "DIVISION":
+                        Console.WriteLine("El resultado de la DIVISION  es:")
+                        R = Num1 / Num2;
+                        Console.WriteLine("{0} * {1} = {2}", Num1, Num2, R);
+
+                        break;
+                }
+                Console.Write("¿Desea Continuar? S/N: ");
+                Respuesta = Console.ReadLine();
+            }
+            while (Respuesta == "s" || Respuesta == "S");
+        }
+    }
+}
