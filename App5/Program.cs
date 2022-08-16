d0fdef50f32a90726184b644677edb8b52c18978
Hazel Granados <hazelbgranados@gmail.com>
2022-08-15 20:53:13 -0900

    Programa 5

---
+++ App5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace App5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            do
+            {
+                Console.WriteLine("Ingrese un numero entero (Con 0 se finaliza): ");
+                num = int.Parse(Console.ReadLine());
+
+                if (num >= 100)
+                {
+                    Console.WriteLine("Tiene 3 digitos.");
+                }
+                else if (num >= 10)
+                {
+                    Console.WriteLine("Tiene 2 digitos.");
+                }
+                else
+                {
+                    Console.WriteLine("Tiene 1 digitos.");
+                }
+
+            } while (num != 0);
+        }
+    }
+}
